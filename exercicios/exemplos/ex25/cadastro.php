d148ee633b99e84a51df49082486cc5171bd4975
marciomrd <147844747+marciomrd@users.noreply.github.com>
2024-08-14 18:41:57 -0300

    Update ex25
    Creating form3.html for example 25. Using new input types.

--- exercicios/exemplos/ex25/cadastro.php
+++ exercicios/exemplos/ex25/cadastro.php
@@ -1,3 +1,4 @@
-Arquivo de exemplo para teste de form!
+Os dados do seu form foram enviados!
 Se quiser saber como tratar os dados enviados
 Escolha uma linguagem de programação como PHP (por exemplo)
+para fazer esse tratamento.
